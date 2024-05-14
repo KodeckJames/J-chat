489ad882aec201264d48f42f6d8a381c5f60b13a
Kodeck James <kodeckjames095@gmail.com>
2024-05-14 22:55:15 +0300

    finished left section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
 const Button = () => {
-  return <button className=" bg-blue-500 text-white rounded-3xl">Post</button>
+  return (
+    <>
+      <button className=" bg-purple-600 text-white rounded-3xl py-3 px-4 mt-2 mx-8 flex justify-center ml-0 max-xl:hidden">
+        Post
+      </button>
+      <a href="">
+        <span className=" xl:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#9333ea"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="10"></circle>
+            <line x1="12" y1="8" x2="12" y2="16"></line>
+            <line x1="8" y1="12" x2="16" y2="12"></line>
+          </svg>
+        </span>
+      </a>
+    </>
+  )
 }
 
 export default Button
--- src/sections/Left.jsx
+++ src/sections/Left.jsx
@@ -1,148 +1,227 @@
 import React from 'react'
 import Button from '../components/Button'
 // import Nav from './components/Nav'
+import { useState } from 'react'
 
 const Left = () => {
+  //   const [count, setCount] = useState(0)
+  //   function click() {
+  //     setCount((count += 1))
+  //     return count
+  //   }
   return (
     <>
-      <div className=" bg-black pl-4 flex flex-col min-h-screen w-[20%]">
-        <img
-          src="/images/logo.png
+      <header className=" bg-black pl-4 flex flex-col min-h-screen w-[20%] max-xl:items-center ">
+        <a href="">
+          <img
+            src="/images/logo.png
               "
-          alt="logo"
-          width={50}
-          height={50}
-          className=" rounded-full pb-6"
-        />
-        <div className=" flex flex-col space-y-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            alt="logo"
+            width={30}
+            height={30}
+            className=" rounded-full pb-4 mt-2 cursor-pointer xl:ml-4"
+          />
+        </a>
+        <div className=" flex flex-col ">
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
-            <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
+                <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Home
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <line x1="4" y1="9" x2="20" y2="9"></line>
-            <line x1="4" y1="15" x2="20" y2="15"></line>
-            <line x1="10" y1="3" x2="8" y2="21"></line>
-            <line x1="16" y1="3" x2="14" y2="21"></line>
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Explore
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path>
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path>
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Notifications
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-            <polyline points="22,6 12,13 2,6"></polyline>
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                <polyline points="22,6 12,13 2,6"></polyline>
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Messages
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Grok
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <line x1="8" y1="6" x2="21" y2="6"></line>
-            <line x1="8" y1="12" x2="21" y2="12"></line>
-            <line x1="8" y1="18" x2="21" y2="18"></line>
-            <line x1="3" y1="6" x2="3.01" y2="6"></line>
-            <line x1="3" y1="12" x2="3.01" y2="12"></line>
-            <line x1="3" y1="18" x2="3.01" y2="18"></line>
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="8" y1="6" x2="21" y2="6"></line>
+                <line x1="8" y1="12" x2="21" y2="12"></line>
+                <line x1="8" y1="18" x2="21" y2="18"></line>
+                <line x1="3" y1="6" x2="3.01" y2="6"></line>
+                <line x1="3" y1="12" x2="3.01" y2="12"></line>
+                <line x1="3" y1="18" x2="3.01" y2="18"></line>
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Lists
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
-            <circle cx="12" cy="10" r="3" />
-            <circle cx="12" cy="12" r="10" />
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="34"
-            height="34"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
+                <circle cx="12" cy="10" r="3" />
+                <circle cx="12" cy="12" r="10" />
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              Profile
+            </div>
+          </a>
+          <a
+            className=" flex hover:bg-gray-800 rounded-full p-3 hover:m-auto"
+            href=""
           >
-            <circle cx="12" cy="12" r="1"></circle>
-            <circle cx="19" cy="12" r="1"></circle>
-            <circle cx="5" cy="12" r="1"></circle>
-          </svg>
+            <span title="Home" className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="34"
+                height="34"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="1"></circle>
+                <circle cx="19" cy="12" r="1"></circle>
+                <circle cx="5" cy="12" r="1"></circle>
+              </svg>
+            </span>
+            <div className=" text-white mb-0 max-xl:hidden  ml-4 text-2xl">
+              More
+            </div>
+          </a>
         </div>
         <Button />
-      </div>
+      </header>
     </>
   )
 }
