bf47dd51db3af8e84862d21905dacabe154fd61d
Kodeck James <kodeckjames095@gmail.com>
2024-05-14 11:55:39 +0300

    second commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = () => {
-  return <button className=" bg-blue-500 text-white rounded-3xl">Tweet</button>
+  return <button className=" bg-blue-500 text-white rounded-3xl">Post</button>
 }
 
 export default Button
