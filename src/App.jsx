cfc2b2f8d4d4826805051388fab2d494998745f4
Kodeck James <kodeckjames095@gmail.com>
2024-05-15 22:01:09 +0300

    center section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Left from './sections/Left'
+import Center from './sections/Center'
 
 const App = () => {
   return (
     <>
-      <main>
-        <Left />
+      <main className=" flex">
+        <>
+          <Left />
+        </>
+        <>
+          <Center />
+        </>
       </main>
     </>
   )
---
+++ src/sections/Center.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Center = () => {
+  return (
+    <>
+      <main className=" bg-black lg:w-[50%] max-lg:w-[70%] border-gray-700 border-x-2 "></main>
+    </>
+  )
+}
+
+export default Center
