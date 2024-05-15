4c1ac90d1f4674c2e803318313da96d42d14dfd5
Kodeck James <kodeckjames095@gmail.com>
2024-05-16 00:33:36 +0300

    break-points complete

--- src/sections/Center.jsx
+++ src/sections/Center.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Center = () => {
   return (
     <>
-      <main className=" bg-black lg:w-[50%] max-lg:w-[70%] border-gray-700 border-x-2 "></main>
+      <main className=" bg-black lg:w-[50%] max-lg:w-[70%] max-sm:w-[100%] border-gray-700 border-x-2 "></main>
     </>
   )
 }
