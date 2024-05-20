ff7d425bf4da23c7880b9473617621e054f57882
Kodeck James <kodeckjames095@gmail.com>
2024-05-20 15:30:09 +0300

    search bar initiated

--- src/mini_sections/SearchBar.jsx
+++ src/mini_sections/SearchBar.jsx
@@ -2,19 +2,14 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div class="flex items-center justify-center h-screen">
-      <form class="flex items-center space-x-2">
+    <div className=" flex justify-start mt-1 ml-4 w-full mr-5">
+      <form action="" className=" w-full mr-28">
         <input
           type="search"
-          placeholder="Search..."
-          class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className=" bg-gray-600 flex items-start  mx-0 rounded-full px-3 py-1 justify-center w-full active:border-purple-600 border-purple-600
+        "
+          placeholder="Search"
         />
-        <button
-          type="submit"
-          class="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          Search
-        </button>
       </form>
     </div>
   )
